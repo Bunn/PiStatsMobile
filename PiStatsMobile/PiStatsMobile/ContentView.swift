7971a36cafb913ff1bd3096ccdb4e6530fb41243
Fernando Bunn <bunn@idevzilla.com>
2020-07-02 19:07:37 +0100

    initial commit

---
+++ PiStatsMobile/PiStatsMobile/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PiStatsMobile
+//
+//  Created by Fernando Bunn on 02/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PiStatsMobile/PiStatsMobile/PiStatsMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiStatsMobileApp.swift
+//  PiStatsMobile
+//
+//  Created by Fernando Bunn on 02/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct PiStatsMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
