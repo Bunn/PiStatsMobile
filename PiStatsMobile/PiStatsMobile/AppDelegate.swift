fb8e436925f44dfeb3acf27425c41db301f41422
Fernando Bunn <bunn@idevzilla.com>
2020-07-11 12:01:34 +0100

    WIP: Background fetch
    Currently there's no way to schedule a background fetch because scenePhase is not working

--- PiStatsMobile/PiStatsMobile/AppDelegate.swift
+++ PiStatsMobile/PiStatsMobile/AppDelegate.swift
@@ -6,7 +6,45 @@
 //
 
 import UIKit
+import BackgroundTasks
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // TODO, I can't set the background fetch since scenePhase is not working https://twitter.com/fcbunn/status/1281900924798226432?s=21
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: "dev.bunn.PiStatsMobile.updatePiholes", using: nil) { task in
+//            self.handleAppRefresh(task: task as! BGAppRefreshTask)
+//        }
+        
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("YEA")
+    }
+    /*
+     func scheduleAppRefresh() {
+     let request = BGAppRefreshTaskRequeste(identifier: dev.bunn...)
+     request.earlierBeginDate = Date(....
+     
+     do {
+     try BGTaskScheduler.shared.submit(request)
+     } catch {
+     bleh
+     }
+     */
+    private func handleAppRefresh(task: BGAppRefreshTask) {
+        //scheduleAppRefresh
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        
+        //let operations = Operations.getOperationsToFetchLatestEntries(....
+        task.expirationHandler = {
+            queue.cancelAllOperations()
+        }
+        
+        //let lastOperation = operations.last!
+            //lastOperation.completionBlock = { task.setTaskCompleted(success: !lastOperation.isCancelled)
+        
+    }
 }
--- PiStatsMobile/PiStatsMobile/PiStatsMobileApp.swift
+++ PiStatsMobile/PiStatsMobile/PiStatsMobileApp.swift
@@ -27,13 +27,35 @@ final class DataModel: ObservableObject {
 @main
 struct PiStatsMobileApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject var dataModel = DataModel()
     
     var body: some Scene {
+        MainAppScene()
+ 
+    }
+}
+
+struct MainAppScene: Scene {
+    @Environment(\.scenePhase) private var phase
+    @StateObject var dataModel = DataModel()
+
+    var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(dataModel.piholeProviderListManager)
-                .environmentObject(dataModel.userPreferences)
+         ContentView()
+            .environmentObject(dataModel.piholeProviderListManager)
+            .environmentObject(dataModel.userPreferences)
+
+        }
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            case .active:
+                print("a")
+            case .inactive:
+                print("ab")
+            case .background:
+                print("ac")
+            @unknown default: break
+                // Fallback for future cases
+            }
         }
     }
 }
