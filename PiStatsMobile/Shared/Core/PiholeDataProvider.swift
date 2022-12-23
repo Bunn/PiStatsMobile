bbe86ba807e847487fb740c3afaf2472bee92714
Fernando Bunn <bunn@idevzilla.com>
2022-12-23 11:21:25 +0000

    Update SwiftHole - Pi-hole v5.18 compatibility

--- PiStatsMobile/Shared/Core/PiholeDataProvider.swift
+++ PiStatsMobile/Shared/Core/PiholeDataProvider.swift
@@ -218,6 +218,8 @@ class PiholeDataProvider: ObservableObject, Identifiable {
             return  UIConstants.Strings.Error.invalidResponse
         case .invalidAPIToken:
             return  UIConstants.Strings.Error.invalidAPIToken
+        case .cantAddNewListItem(_):
+            return UIConstants.Strings.Error.cantAddNewItem
         }
     }
     
